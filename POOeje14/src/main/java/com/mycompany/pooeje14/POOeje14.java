b27b067c69ede3bf1c9596940b28917c1f40bbac
Daniel Rincon <drinconva@unal.edu.co>
2026-09-17 22:00:45 -0500

    TALLER 1 POO

--- POOeje14/src/main/java/com/mycompany/pooeje14/POOeje14.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.pooeje14;
-
-public class POOeje14 {
-
-    public static void main(String[] args) {
-        System.out.println("");
-    }
-}
--- POOeje17/src/main/java/com/mycompany/pooeje17/POOeje17.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.pooeje17;
-
-public class POOeje17 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
